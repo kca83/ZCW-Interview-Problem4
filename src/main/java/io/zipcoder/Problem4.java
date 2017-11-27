3aaaf40713b88962497ffb40345eecfa542556a3
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-11-27 15:37:27 -0500

    Katherine Anderson commit

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,54 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Problem4 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your string to test if it can be made into a palindrome?");
+        String input = scan.nextLine();
+        System.out.println("Can your string be a palindrome?");
+        Problem4 problem4 = new Problem4();
+        System.out.println(problem4.hasPalindrome(input));
+    }
+
+    public String hasPalindrome(String input) {
+        HashMap<Character, Integer> numChars = countNumberOfEachCharacter(input);
+
+        if(canBePalindrome(numChars)) {
+            return "YES";
+        }
+        else {
+            return "NO";
+        }
+    }
+
+    public HashMap<Character, Integer> countNumberOfEachCharacter(String input) {
+        HashMap<Character, Integer> numChars = new HashMap<Character, Integer>();
+        for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if(numChars.containsKey(c)) {
+                numChars.put(c, numChars.get(c) + 1);
+            }
+            else {
+                numChars.put(c, 1);
+            }
+        }
+        return numChars;
+    }
+
+    public boolean canBePalindrome(HashMap<Character, Integer> numChars) {
+        int numOddCounts = 0;
+        for(char c : numChars.keySet()) {
+            if(numChars.get(c) % 2 != 0) {
+                numOddCounts++;
+            }
+            if(numOddCounts > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,80 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 public class Problem4Test {
+
+    Problem4 problem4;
+
+    @Before
+    public void initialize() {
+        problem4 = new Problem4();
+    }
+
+    @Test
+    public void hasPalindromeTest() {
+        String input = "aaabbbb";
+
+        String expected = "YES";
+        String actual = problem4.hasPalindrome(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void doesNotHavePalindromeTest() {
+        String input = "cdefghmnopqrstuvw";
+
+        String expected = "NO";
+        String actual = problem4.hasPalindrome(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void hasPalindromeTest2() {
+        String input = "cdcdcdcdeeeef";
+
+        String expected = "YES";
+        String actual = problem4.hasPalindrome(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void countNumberOfEachCharacterTest() {
+        String input = "aaabbbb";
+
+        HashMap<Character, Integer> expected = new HashMap<Character, Integer>();
+        expected.put('a', 3);
+        expected.put('b', 4);
+
+        HashMap<Character, Integer> actual = problem4.countNumberOfEachCharacter(input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void canBePalindromeTest() {
+        HashMap<Character, Integer> input = new HashMap<Character, Integer>();
+        input.put('c', 1);
+        input.put('a', 4);
+        input.put('t', 10);
+
+        Assert.assertTrue(problem4.canBePalindrome(input));
+    }
+
+    @Test
+    public void cannotBePalindromeTest() {
+        HashMap<Character, Integer> input = new HashMap<Character, Integer>();
+        input.put('c', 1);
+        input.put('a', 3);
+        input.put('t', 10);
+
+        Assert.assertFalse(problem4.canBePalindrome(input));
+    }
 }
